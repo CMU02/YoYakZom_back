ad99f9e90b8a625f29c7bcc6fdad1bff47669253
CMU02 <Hyeonjun1968@naver.com>
2024-11-13 19:42:28 +0900

    - FEAT : CORS 설정 추가

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const corsOptions: CorsOptions = {
+    origin: ['https://yoyakzom.com', 'http://localhost:3000', 'http://localhost:5713'], // 허용할 출처 설정
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // 허용할 HTTP 메서드
+    credentials: true, // 인증 정보(Cookies, Authorization 헤더 등) 허용
+  };
+
+  app.enableCors(corsOptions);
+
   await app.listen(process.env.PORT ?? 7002);
 }
 bootstrap();
